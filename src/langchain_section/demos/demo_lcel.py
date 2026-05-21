61f27cfffcb23b65c3bcd4c3746e91738b38c45e
Ricardo Cuéllar <rcuellarsn@gmail.com>
2026-05-21 00:58:03 -0600

    Add main function in demo_lcel

---
+++ src/langchain_section/demos/demo_lcel.py
@@ -0,0 +1,14 @@
+"""Demo LCEL"""
+from src.langchain_section.core.llm import get_llm
+from src.langchain_section.config.settings import settings
+
+llm = get_llm()
+
+
+# código aquí
+
+
+if __name__ == "__main__":
+    print("="*60)
+    print("LangChain LCEL - Fundamentos")
+    print("="*60)
