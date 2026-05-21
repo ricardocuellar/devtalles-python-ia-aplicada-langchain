9d4698b28abc2a7ab1f27bf32db0b6a4e3f460a5
Ricardo Cuéllar <rcuellarsn@gmail.com>
2026-05-21 07:07:36 -0600

    Add steps inspection

--- src/langchain_section/demos/demo_lcel.py
+++ src/langchain_section/demos/demo_lcel.py
@@ -1,8 +1,9 @@
 """Demo LCEL"""
-from src.langchain_section.core.llm import get_llm
-from src.langchain_section.config.settings import settings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from src.langchain_section.core.llm import get_llm
+from src.langchain_section.config.settings import settings
+
 
 llm = get_llm()
 
@@ -34,8 +35,41 @@ def demo_simple_chain() -> None:
     print()
 
 
+def demo_steps_inspection() -> None:
+    """Invoca cada componente"""
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Eres un asistente técnico."),
+        ("human", "{pregunta}")
+    ])
+
+    parser = StrOutputParser()
+
+    input_message = {"pregunta": "¿Qué es una API REST?"}
+
+    # Paso 1 prompt
+    messages = prompt.invoke(input_message)
+    print("Paso 1 - Prompt output")
+    print(f" Tipo: {type(messages).__name__}")
+    print(f" Mensajes: {messages.messages}")
+
+    # Paso 2 LLM -> AIMessage
+    ai_message = llm.invoke(messages)
+    print("Paso 2 - LLM output")
+    print(f" Tipo: {type(ai_message).__name__}")
+    print(f" AIMessage: {ai_message} ")
+    print(f" Contenido: {ai_message.content[:100]}...")
+
+    # Paso 3: Parser extraer texto
+    text = parser.invoke(ai_message)
+    print("Paso 3 - Parser output")
+    print(f" Tipo: {type(text).__name__}")
+    print(f" Texto: {text[:100]}...")
+    print()
+
+
 if __name__ == "__main__":
     print("="*60)
     print("LangChain LCEL - Fundamentos")
-    demo_simple_chain()
+    # demo_simple_chain()
+    demo_steps_inspection()
     print("="*60)
